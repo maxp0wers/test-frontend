d09e0aeb3b6a53a0a8d50bec1ab51cabbb9dc766
Francisco Salido <psalido@gmail.com>
2020-12-01 18:56:04 +0100

    Improve table template and add loading state

--- projects/ng-lottoland/src/lib/table/table.component.ts
+++ projects/ng-lottoland/src/lib/table/table.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class TableComponent implements OnInit {
   @Input() columns: string[];
   @Input() data;
+  @Input() loading: boolean;
 
   constructor() { }
 
